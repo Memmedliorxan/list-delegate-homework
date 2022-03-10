246fd1575eaa2a6fc3c6db45f4790589c84ca6a0
Memmedliorxan <orkhanama@code.edu.az>
2022-03-10 23:55:05 +0400

    :first commit

---
+++ list delegate task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using list_delegate_task1.models;
+using System;
+using System.Collections.Generic;
+
+namespace list_delegate_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            library.AddBook("Titanic", "Home alone", 110);
+            library.AddBook("Animal", "Vuqar", 100);
+            library.AddBook("History", "Aydin", 350);
+            library.AddBook("Physics", "samir", 170);
+            library.AddBook("Hello", "Nadir", 75);
+            //library.GetBook();
+            //library.RemoveByNo("Ti1");
+            //library.RemoveAllBookByName("Hello");
+            //library.GetBook();
+            //ShowBook(library.SearchBooks("Animal"));
+            //ShowBook(library.FindAllBooksByPageCountRange(70, 110));
+        }
+        static void ShowBook(List<Book> books)
+        {
+            foreach (var item in books)
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine(item);
+                Console.WriteLine("----------");
+            }
+
+        }
+    }
+}
---
+++ list delegate task1/models/book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace list_delegate_task1.models
+{
+    class Book
+    {
+        private static int _count = 1;
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+        public string Code { get; set; }
+
+        public Book (string name, string authorName, int pageCount)
+        {
+            Name = name;
+            AuthorName = authorName;
+            PageCount = pageCount;
+            Code = $"{name[0..2].ToUpper()}{_count}";
+            _count++;
+        }
+        public override string ToString()
+        {
+            return $"Book Code: {Code}\n" +
+                $"Book Name: {Name}\n" +
+                $"Author Name: {AuthorName}\n" +
+                $"Page Count: {PageCount}";
+        }
+    }
+}
